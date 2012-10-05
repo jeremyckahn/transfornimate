6a6daf06f2ef4f3496dfa5ef03689466f90a2eb5
Jeremy Kahn <jeremyckahn@gmail.com>
2012-10-04 20:49:34 -0700

    Queries the currently hovered element.

--- src/init.js
+++ src/init.js
@@ -1,1 +1,16 @@
-console.log('Hello!');
+/**
+ * @param {Object} info
+ * @param {Object} tab
+ */
+function onClickHandler (info, tab) {
+  console.log(info, tab);
+}
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    'title': 'Animate this element'
+    ,'id': 'init'
+  });
+});
